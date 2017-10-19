5f5d1e2d58dfd641666278c5aa4317364980115c
martiuripariu <martiuripariu@gmail.com>
2017-10-19 20:44:21 -0200

    criação dos modulos motinoramento e relatorio

--- lifeboxproject/settings.py
+++ lifeboxproject/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 
-INSTALLED_APPS = [    
+INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'bootstrap3',
     'cadastro',
+    'monitoramento',
+    'relatorios',
 ]
 
 MIDDLEWARE = [
--- lifeboxproject/urls.py
+++ lifeboxproject/urls.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('account.urls')),
+    url(r'^monitoramento/', include('monitoramento.urls')),
+    url(r'^relatorios/', include('relatorios.urls')),
     url(r'^account/', include('account.urls')),
     url(r'^cadastro/', include('cadastro.urls')),
     url(r'social-auth/', include('social_django.urls', namespace='social'))
